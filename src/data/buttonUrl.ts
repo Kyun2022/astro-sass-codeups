f284acbb04b869b754a23992651d7107993b6d3b
kyun2022 <fm13071303@rx.tnc.ne.jp>
2024-07-11 22:20:50 +0900

    ✨ Sassの設定が大変だった

---
+++ src/data/buttonUrl.ts
@@ -0,0 +1,14 @@
+interface Props {
+	url?: string;
+}
+
+export const buttonUrl = [
+	{ url: 'campaign.html' },
+	{ url: 'aboutus.html' },
+	{ url: 'information.html' },
+	{ url: 'blog.html' },
+	{ url: 'voice.html' },
+	{ url: 'price.html' },
+	{ url: 'faq.html' },
+	{ url: 'contact.html' },
+];
--- src/data/swiperData.ts
+++ src/data/swiperData.ts
@@ -1,6 +1,15 @@
+import slider1 from 'src/assets/slider_2.jpg';
+import slider2 from 'src/assets/slider_3.jpg';
+import slider3 from 'src/assets/slider_4.jpg';
+import slider4 from 'src/assets/slider_1.jpg';
+import slider5 from 'src/assets/slider_2.jpg';
+import slider6 from 'src/assets/slider_3.jpg';
+import slider7 from 'src/assets/slider_4.jpg';
+import slider8 from 'src/assets/slider_1.jpg';
+
 export const swiperData = [
 	{
-		src: 'src/assets/slider_2.jpg',
+		src: slider1,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: '体験ダイビング',
 		title: '貸切体験ダイビング',
@@ -9,7 +18,7 @@ export const swiperData = [
 		newPrice: '\u00A518\u002C000',
 	},
 	{
-		src: 'src/assets/slider_3.jpg',
+		src: slider2,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: '体験ダイビング',
 		title: 'ナイトダイビング',
@@ -18,7 +27,7 @@ export const swiperData = [
 		newPrice: '\u00A58\u002C000',
 	},
 	{
-		src: 'src/assets/slider_4.jpg',
+		src: slider3,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: 'ファンダイビング',
 		title: '貸切ファンダイビング',
@@ -27,7 +36,7 @@ export const swiperData = [
 		newPrice: '\u00A516\u002C000',
 	},
 	{
-		src: 'src/assets/slider_1.jpg',
+		src: slider4,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: 'ライセンス講習',
 		title: 'ライセンス取得',
@@ -36,7 +45,7 @@ export const swiperData = [
 		newPrice: '\u00A546\u002C000',
 	},
 	{
-		src: 'src/assets/slider_2.jpg',
+		src: slider5,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: '体験ダイビング',
 		title: '貸切体験ダイビング',
@@ -45,7 +54,7 @@ export const swiperData = [
 		newPrice: '\u00A518\u002C000',
 	},
 	{
-		src: 'src/assets/slider_3.jpg',
+		src: slider6,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: '体験ダイビング',
 		title: 'ナイトダイビング',
@@ -54,7 +63,7 @@ export const swiperData = [
 		newPrice: '\u00A58\u002C000',
 	},
 	{
-		src: 'src/assets/slider_4.jpg',
+		src: slider7,
 		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
 		label: 'ファンダイビング',
 		title: '貸切ファンダイビング',
@@ -63,12 +72,12 @@ export const swiperData = [
 		newPrice: '\u00A516\u002C000',
 	},
 	{
-      src: 'src/assets/slider_1.jpg',
-      alt: 'さまざまな色をした魚が群れで泳いでいる様子',
-      label: 'ライセンス講習',
-      title: 'ライセンス取得',
-      subTitle: '全部コミコミ(お一人様)',
-      oldPrice: '\u00A556\u002C000',
-      newPrice: '\u00A546\u002C000',
-   },
+		src: slider8,
+		alt: 'さまざまな色をした魚が群れで泳いでいる様子',
+		label: 'ライセンス講習',
+		title: 'ライセンス取得',
+		subTitle: '全部コミコミ(お一人様)',
+		oldPrice: '\u00A556\u002C000',
+		newPrice: '\u00A546\u002C000',
+	},
 ];
